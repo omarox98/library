d915195fa25560d90f17c718b3811980b5621f9d
omarox98 <omaraizab98@gmail.com>
2020-01-07 16:31:59 +0100

    Update Menu

--- src/views/Menu.java
+++ src/views/Menu.java
@@ -6,31 +6,38 @@ public class Menu {
     private static Scanner sc = new Scanner(System.in);
 
     public static int main() {
+        System.out.println("---------------------------------");
         System.out.println("1- Manage books");
         System.out.println("2- Manage students");
         System.out.println("3- To borrow a book");
         System.out.println("4- Submit a book");
         System.out.println("5- Load data from a file to the database");
         System.out.println("6- Save data from database to CSV file");
+        System.out.println("0- Exit");
+        System.out.println("---------------------------------");
         return sc.nextInt();
     }
 
     public static int books() {
+        System.out.println("---------------------------------");
         System.out.println("1- List books");
         System.out.println("2- Fill the stock of a book");
         System.out.println("3- Add a book");
         System.out.println("4- Edit a book");
         System.out.println("5- Delete a book");
         System.out.println("0- Back");
+        System.out.println("---------------------------------");
         return sc.nextInt();
     }
 
     public static int students() {
+        System.out.println("---------------------------------");
         System.out.println("1- List students");
         System.out.println("2- Add a student");
         System.out.println("3- Edit a student");
         System.out.println("4- Delete a student");
         System.out.println("0- Back");
+        System.out.println("---------------------------------");
         return sc.nextInt();
     }
 }
